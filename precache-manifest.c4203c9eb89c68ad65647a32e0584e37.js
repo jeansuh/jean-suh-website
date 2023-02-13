221c0cec9995e022e6ded4172b5edc94ac0b2f60
Jean Suh <jeansuh@Jeans-MacBook-Pro-2.local>
2023-02-13 18:58:59 -0500

    Updates

---
+++ precache-manifest.c4203c9eb89c68ad65647a32e0584e37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bace316961f34ab7c4",
+    "url": "/jeansuh/jean-suh-website/static/css/main.938d39bd.chunk.css"
+  },
+  {
+    "revision": "e4bace316961f34ab7c4",
+    "url": "/jeansuh/jean-suh-website/static/js/main.1591571c.chunk.js"
+  },
+  {
+    "revision": "32795bbca6d26dc4a75e",
+    "url": "/jeansuh/jean-suh-website/static/js/runtime~main.76b33495.js"
+  },
+  {
+    "revision": "0335c0a13fecfb151ee5",
+    "url": "/jeansuh/jean-suh-website/static/js/2.49cb00a6.chunk.js"
+  },
+  {
+    "revision": "089bc876b02613239fa5",
+    "url": "/jeansuh/jean-suh-website/static/js/3.b7c4cbb6.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/jeansuh/jean-suh-website/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "aa26a359d6ba0e3dd88aa0ce68064900",
+    "url": "/jeansuh/jean-suh-website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5296406368e5dc08e318cf426046a5cb.js"
+  "/jeansuh/jean-suh-website/precache-manifest.c4203c9eb89c68ad65647a32e0584e37.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/jeansuh/jean-suh-website/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
